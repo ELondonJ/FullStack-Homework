0c523cba0a6bc86d5512736055c769019bd270c2
ELondonJ <ljoseph@pdx.edu>
2022-10-11 13:38:32 -0700

    solution for 04-api complete

--- hw2/04-api.js
+++ hw2/04-api.js
@@ -1,7 +1,39 @@
-/** Exercise 04 - API **/
+/** Exercise 04 - API * */
+const url = 'https://restcountries.com/v3.1/all?fields=name,population';
 
-const url = 'https://restcountries.com/v3.1/all';
+const addToDom = (country) => {
+  const toAdd = `${
+    country.name.common
+  } - ${country.population.toLocaleString()}`;
+  const results = document.getElementById('results');
+  const item = document.createElement('li');
 
-// Add your code here
+  item.className = 'h5';
+  item.textContent = toAdd;
+  results.appendChild(item);
+};
+
+const getData = async function(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    data.sort((a, b) => {
+      const ca = a.name.common;
+      const cb = b.name.common;
+
+      if (ca < cb) return -1;
+      if (ca > cb) return 1;
+
+      return 0;
+    });
+    data.forEach((country) => {
+      addToDom(country);
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log('complete');
+  }
+};
 
 getData(url);
