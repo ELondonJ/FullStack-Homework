28792c30596490aeb8b610c6c56a709ad7dd2f92
ELondonJ <ljoseph@pdx.edu>
2022-11-05 13:58:44 -0700

    hw4 sessions complete

--- hw4/02-sessions.js
+++ hw4/02-sessions.js
@@ -6,10 +6,46 @@ const port = process.env.PORT || 5001;
 // Add your code here
 
 // Use the express-session module
-app.use(/** ... */);
+app.use(
+  session({
+    secret: 'secret-key',
+    resave: 'false',
+    saveUninitialized: 'true'
+  })
+);
 
-app.get('/', (req, res) => {
-  // Add your code here
+const getRoutes = (routes) => {
+  let result = '';
+  if (routes === undefined) return result;
+  routes.forEach(
+    (elem) => (result += `<li style ="margin-left: 15px">\t${elem}</li>`)
+  );
+
+  return result;
+};
+app.get('*', (req, res) => {
+  console.log(req.session.urlList);
+  const routes = getRoutes(req.session.urlList, 1);
+  if (req.session.urlList) {
+    console.log(req.session.urlList, 2);
+    req.session.urlList.push(req.url);
+  } else {
+    console.log(req.session.urlList, 3);
+    req.session.urlList = [req.url];
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  console.log(routes);
+  res.write(`<br><span">Currently on route: ${req.url}</span><br><br>`);
+  if (routes === '') {
+    res.write(`<span">Welcome to http://localhost:${port}</span>`);
+  } else {
+    res.write(
+      `<ul style = "list-style-type: none; padding: 0">Previously Visited: ${routes}</ul>`
+    );
+  }
+  res.end();
 });
 
 app.listen(port, () => {
