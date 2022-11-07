3459549783d2c7b20c9c284aec15d1a875f47b7e
ELondonJ <ljoseph@pdx.edu>
2022-11-06 19:15:04 -0800

    hw4 form complete

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5001;
 
 // Use middleware static() to serve all static files in the given folder
@@ -9,8 +10,27 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
 // POST request
-app.post('', (req, res) => {
+app.post('/submit', (req, res) => {
+  const name = req.body.name === '' ? 'not provided' : req.body.name;
+  const email = req.body.email === '' ? 'not provided' : req.body.email;
+  const feedback =
+    req.body.feedback === '' ? 'not provided' : req.body.feedback;
+  const newsletter =
+    req.body.newsletter === undefined
+      ? 'No, do not sign me up'
+      : 'Yes, sign me up';
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(`<span> Name: ${name} </span><br>`);
+  res.write(`<span>Email: ${email} </span><br>`);
+  res.write(`<span>Feedback: ${feedback}</span><br>`);
+  res.write(`<span>Newsletter: ${newsletter}</span><br>`);
+  res.end();
   // Add your code here
+});
+
+app.get('/', (req, res) => {
+  res.status(200);
+  res.sendFile(path.join(`${__dirname}/public/index.html`));
 });
 
 app.listen(port, () => {
